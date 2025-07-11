82ad2e49647a97bc5bbe0ad05b57f336987d6f42
N.Upendhar <80301044+Upendhar10@users.noreply.github.com>
2025-07-11 16:26:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+
+const form = document.getElementById('myForm');
+
+form.addEventListener("submit" , function (event){
+	event.preventDefault();
+
+	let firstName = document.getElementById('firstName').value;
+	let lastName = document.getElementById('lastName').value;
+	let phone = document.getElementById('phone').value;
+	let email = document.getElementById('email').value;
+
+	alert(
+		`FirstName: ${firstName} LastName: ${lastName} Phone Number: ${phone} Email ID: ${email}` 
+	)
+
+	form.reset();
+})
